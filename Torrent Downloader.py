f59395c06e2c3cdfd3dcc47a7a42f5da6314c851
Soumendra kumar sahoo <soumendrakumar77@gmail.com>
2016-04-24 02:12:50 +0530

    Now schedule it once till the end of GOT season 6
    If you schedule it by windows scheduler, you just need to change the code only once in the beginning. Just terminate the scheduler after the season finale.

--- Torrent Downloader.py
+++ Torrent Downloader.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 import requests
 import subprocess
 import time
-from sms import texting
-from email_code import sendemail
-path = "C:\Users\Soumendra\AppData\Roaming\uTorrent\uTorrent.exe" #uTorrent path
+from sms import texting #imports from the other file attached along with it
+from email_code import sendemail #imports from the other file attached along with it
+from jdcal import gcal2jd
+path = "C:\Users\username\AppData\Roaming\uTorrent\uTorrent.exe" #uTorrent path change accordingly
 def get_page(url):
     hdr={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
     try:
@@ -55,17 +56,46 @@ def go_to_torrent_page(page):
     end_quote=page.find('"',start_quote+1)
     url=page[start_quote+1:end_quote]
     return url
-        
+
+def datecalc():
+    dtime = [int(x) for x in time.strftime('%Y %m %d').split()]    
+    jd = int(gcal2jd(dtime[0], dtime[1], dtime[2])[1])    
+    std = 57503
+    if std >= jd > std + 7 :
+        episode = "01"
+    elif std + 7 >= jd > std + 14:
+        episode = "02"
+    elif std + 14 >= jd > std + 21:
+        episode = "03"
+    elif std + 21 >= jd > std + 28:
+        episode = "04"
+    elif std + 28 >= jd > std + 35:
+        episode = "05"
+    elif std + 35 >= jd > std + 42:
+        episode = "06"
+    elif std + 42 >= jd > std + 49:
+        episode = "07"
+    elif std + 49 >= jd > std + 56:
+        episode = "08"
+    elif std + 56 >= jd > std + 63:
+        episode = "09"
+    elif std + 63 >= jd > std + 100:
+        episode = "10"
+    else:
+        episode = "00"
+    return episode
+    
 def main(Flag):
-    searchItem=raw_input("Enter the torrent you wish to search:")
-#     searchItem="Game of thrones S06E09"
+    # searchItem=raw_input("Enter the torrent you wish to search:")
+    episode = datecalc()
+    searchItem="Game of thrones S06E"+episode    
     completeUrl="http://torrentz.com/search?q="+searchItem.replace(" ","+")
     pageContent=get_page(completeUrl)    
     newUrl="http://torrentz.com"+ str(go_to_next_page(pageContent))
     nextPageContent=get_page(newUrl)
     if nextPageContent == "No":
-        print time.strftime('%X'), ":  File NOT avaiable yet"                
-        return "N"
+        print time.strftime('%X'), ":  File is NOT avaiable yet"                        
+        return "N", ""
     finalUrl=go_to_torrent_page(nextPageContent)    
     if finalUrl==False:
         print "Can not process Ahead as URL not found"
@@ -87,11 +117,11 @@ if __name__ == "__main__":
         if Flag == 'N':
             Flag, title = main(Flag)
             if Flag != 'Y':
-                time.sleep(1800)                
+                time.sleep(900)                #Executes in every 15 minutes if file unavailable
         else:
             print time.strftime('%X'),":  Torrent downloaded"
-            email = 'xxxxxxxxy3@gmail.com xxxxxxxxxilu@yahoo.com '
-            number = '+91XXXXXXXXX +91XXXXXXXXX'
+            email = 'xxxxxxxy3@gmail.com xxxxxxxxxxilu@yahoo.com xxxxxxx@engineer.com xxxxxx@programmer.net'
+            number = '+91XXXXXXXXXX +91XXXXXXXXX'
             message = 'Subject: Torrent status. \nThe file ' + title + ' is available now. Downloading it'
             texting(message, number)
             sendemail(email, message)
