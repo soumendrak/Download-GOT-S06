7a3f8b971a45c716f1f336346b43831d715dcf1c
Soumendra kumar sahoo <soumendrakumar77@gmail.com>
2016-04-25 09:19:06 +0530

    PEP8 standards updated

--- email_code.py
+++ email_code.py
@@ -1,13 +1,17 @@
-from smtplib import SMTP 
+from smtplib import SMTP, SMTPRecipientsRefused
 obj = SMTP('smtp.gmail.com', 587)
 obj.ehlo()
 obj.starttls()
-obj.login('xxxxxxxxxxxxxx7@gmail.com', 'password')
+obj.login('xxxxxxxxxx@gmail.com', 'password')
+
 
 def sendemail(emails, message):
     emails = emails.split(' ')
     N = len(emails)
     for i in range(N):
-        print obj.sendmail('soumendra@programmer.net', str(emails[i]), message)
-        print emails[i]                   
+        try:
+            obj.sendmail('xxxxxxxx@gmail.com', str(emails[i]), message)
+            print emails[i]
+        except SMTPRecipientsRefused:
+            print 'Email to %s could not be sent' % emails[i]
     obj.quit()
